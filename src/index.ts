e9f0c5e61a01225dfb3fc16667f0df2a0cc7bcfc
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-08-15 12:56:44 -0700

    Support different networks (e.g., mainnet vs alfajores)

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,16 @@ interface NFTContract {
   name: string
 }
 
-export const nftContracts: NFTContract[] = [
-  {
-    name: 'Womxn of Celo',
-    contractAddress: '0x50826faa5b20250250e09067e8ddb1afa2bdf910',
-  },
-  {
-    name: 'CeloApesKingdom',
-    contractAddress: '0x1eCD77075F7504bA849d47DCe4cdC9695f1FE942',
-  },
-]
+export const nftContracts: Record<'mainnet' | 'alfajores', NFTContract[]> = {
+  alfajores: [],
+  mainnet: [
+    {
+      name: 'Womxn of Celo',
+      contractAddress: '0x50826faa5b20250250e09067e8ddb1afa2bdf910',
+    },
+    {
+      name: 'CeloApesKingdom',
+      contractAddress: '0x1eCD77075F7504bA849d47DCe4cdC9695f1FE942',
+    },
+  ],
+}
