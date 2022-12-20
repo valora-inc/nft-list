9387b8d5d6da54a0a6542a48abb8852dabd3fd36
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-12-20 09:19:36 -0800

    chore: address validation and enforce a uniform address format

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,23 @@
+import { getAddress } from '@ethersproject/address'
 import Joi from 'joi'
+
+function contractAddressValidation(value: any) {
+  // getAddress throws Error if value isn't a valid address.
+  getAddress(value)
+
+  if (value.toLowerCase() !== value) {
+    throw new Error(`${value} is not a lowercase address (expected ${value.toLowerCase()})`)
+  }
+
+  return value
+}
 
 export const schema = Joi.array().items(
   Joi.object({
     name: Joi.string(),
-    contractAddress: Joi.string().length(42),
+    contractAddress: Joi.string().custom(
+      contractAddressValidation,
+      'Contract address validation',
+    ),
   }),
 )
