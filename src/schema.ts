d7dbb84d88fcdd0987bf4a10f1eb35772d3d55c6
Silas Boyd-Wickizer <silasbw@gmail.com>
2023-05-18 13:20:14 -0700

    chore: update schema validation to require all properties

--- src/schema.ts
+++ src/schema.ts
@@ -16,10 +16,10 @@ function contractAddressValidation(value: any) {
 
 export const schema = Joi.array().items(
   Joi.object({
-    name: Joi.string(),
+    name: Joi.string().required(),
     contractAddress: Joi.string().custom(
       contractAddressValidation,
       'Contract address validation',
-    ),
+    ).required(),
   }),
 )
